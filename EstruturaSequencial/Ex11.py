8a11cec1f180a47b6d206aa1f93321609bb29ddd
Jean Moreira <jeanreal099@gmail.com>
2023-01-12 13:45:42 +0000

    add

--- EstruturaSequencial/Ex11.py
+++ EstruturaSequencial/Ex11.py
@@ -6,3 +6,13 @@
 primeiroNumero = input('Digite um valor inteiro: ')
 segundoNumero = input('Digite outro valor inteiro: ')
 valorReal = input('Digite um valor com casas decimais (real): ')
+
+a = (2 * int(primeiroNumero)) * (int(segundoNumero) / 2)
+b = 3 * int(primeiroNumero) + 3 * float(valorReal)
+c = float(valorReal)**2
+
+print('\nPrimeiro valor: ', primeiroNumero, '\nSegundo valor: ',
+      segundoNumero, '\nTerceiro valor: ', valorReal)
+print('Produto do dobro do primeiro valor com metade do segundo valor: ', a)
+print('Soma do triplo do primeiro valor com o terceiro valor: ', b)
+print('Terceiro valor elevado ao cubo: ', c)
