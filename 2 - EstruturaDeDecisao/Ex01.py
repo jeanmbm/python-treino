ac2c838f6462467764775f96869d9f100e2d3e97
Jean Moreira <jeanreal099@gmail.com>
2023-01-14 18:24:33 +0000

    adicionando comando exercícios

---
+++ 2 - EstruturaDeDecisao/Ex01.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça dois números e imprima o maior deles
+
+primeiroValor = int(input('Digite o primeiro valor: '))
+segundoValor = int(input('Digite o segundo valor: '))
+
+if primeiroValor > segundoValor:
+    print(primeiroValor, ' é o maior valor')
+else:
+    print(segundoValor, ' é o maior valor')
---
+++ 2 - EstruturaDeDecisao/Ex02.py
@@ -0,0 +1,10 @@
+# Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+letra = input('Digite seu sexo - F (feminino) ou M (masculino): ')
+
+if letra == 'F':
+    print('F - Feminino')
+elif letra == 'M':
+    print('M - Masculino')
+else:
+    print('Sexo Inválido')
