c3898adf2e276452935837995e6d27118cac9bb1
Jean Moreira <jeanreal099@gmail.com>
2023-01-16 18:04:57 +0000

    add

--- Exercícios/3 - EstruturaDeRepeticao/Ex05.py
+++ Exercícios/3 - EstruturaDeRepeticao/Ex05.py
@@ -1,1 +1,5 @@
 # Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles. Mostrar no final a soma dos números.
+
+isPassed = False
+while not isPassed:
+    valor = int(input('Digite um valor inteiro: '))
